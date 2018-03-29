947a9f3ff11833ed70d56ca88e39edc25e369e0d
Łukasz Langa <lukasz@langa.pl>
2018-03-28 20:49:12 -0700

    Be explicit in setup.py about Python 3.6+

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     license='MIT',
     py_modules=['pyi'],
     zip_safe=False,
+    python_requires=">=3.6",
     install_requires=['flake8 >= 3.2.1', 'attrs'],
     test_suite='tests.test_pyi',
     classifiers=[
