05d9bb941497f82db41b4434fbee077d963ee205
Akuli <akuviljanen17@gmail.com>
2022-01-14 16:30:03 +0200

    improvements

--- tests/test_pyi.py
+++ tests/test_pyi.py
@@ -8,26 +8,38 @@ import pytest
 
 @pytest.mark.parametrize("path", glob.glob("tests/*.pyi"))
 def test_pyi_file(path):
+    flags = []
     expected_output = ""
-    command = ["flake8", "-j0"]
 
     with open(path) as file:
-        for lineno, line in enumerate(file, start=1):
-            if line.startswith("# flags: "):
-                command.extend(line.split()[2:])
-                continue
+        file_contents = file.read()
 
-            for error_comment in re.findall("# [A-Z][0-9][0-9][0-9][^#]*", line):
-                expected_output += f"{path}:{lineno}: {error_comment[1:].strip()}\n"
+    for lineno, line in enumerate(file_contents.splitlines(), start=1):
+        if line.startswith("# flags: "):
+            flags.extend(line.split()[2:])
+            continue
 
-    command.append(str(path))
+        for error_comment in re.findall("# [A-Z][0-9][0-9][0-9][^#]*", line):
+            expected_output += f"{path}:{lineno}: {error_comment[1:].strip()}\n"
 
-    actual_output = subprocess.run(
-        command,
-        env={**os.environ, "PYTHONPATH": "."},
-        stderr=subprocess.STDOUT,
-        stdout=subprocess.PIPE,
-    ).stdout.decode()
-    actual_output = re.sub(":[0-9]+: ", ": ", actual_output)  # ignore column numbers
-    assert actual_output == expected_output
+    run_results = [
+        # Passing a file on command line
+        subprocess.run(
+            ["flake8", "-j0", *flags, path],
+            env={**os.environ, "PYTHONPATH": "."},
+            stdout=subprocess.PIPE,
+        ),
 
+        # Passing "-" as the file, and reading from stdin instead
+        subprocess.run(
+            ["flake8", "-j0", "--stdin-display-name", path, *flags, "-"],
+            env={**os.environ, "PYTHONPATH": "."},
+            input=file_contents.encode("utf-8"),
+            stdout=subprocess.PIPE,
+        ),
+    ]
+
+    for run_result in run_results:
+        output = run_result.stdout.decode("utf-8")
+        output = re.sub(":[0-9]+: ", ": ", output)  # ignore column numbers
+        assert output == expected_output
