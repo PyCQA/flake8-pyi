56bd4f5b47ae570ab3e4ff2274478a80f0d5272b
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2022-01-15 21:34:38 -0800

    improvements to Assign check (#76)
    Co-authored-by: Shantanu <12621235+hauntsaninja@users.noreply.github.com>

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -4,6 +4,9 @@ Change Log
 unreleased
 ~~~~~~~~~~
 
+* introduce Y093 (require using TypeAlias for type aliases)
+* introduce Y017 (disallows assignments with multiple targets or non-name targets)
+* extend Y001 to cover ParamSpec and TypeVarTuple in addition to TypeVar
 * introduce Y016 (duplicate union member)
 * support Python 3.10
 
--- tests/typevar.pyi
+++ tests/typevar.pyi
@@ -1,4 +1,8 @@
-from typing import TypeVar
+from typing import ParamSpec, TypeVar, TypeVarTuple
 
-T = TypeVar('T')  # Y001 Name of private TypeVar must start with _
-_T = TypeVar('_T')
+T = TypeVar("T")  # Y001 Name of private TypeVar must start with _
+_T = TypeVar("_T")
+P = ParamSpec("P")  # Y001 Name of private ParamSpec must start with _
+_P = ParamSpec("_P")
+Ts = TypeVarTuple("Ts")  # Y001 Name of private TypeVarTuple must start with _
+_Ts = TypeVarTuple("_Ts")
