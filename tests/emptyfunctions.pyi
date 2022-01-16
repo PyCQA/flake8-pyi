0113c01f57febb4101c48e6ac4bcee35fd199eab
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2022-01-16 07:28:52 -0800

    check async functions like normal functions
    Fixes #74

--- tests/emptyfunctions.pyi
+++ tests/emptyfunctions.pyi
@@ -21,3 +21,19 @@ def returning(x: int) -> float:
 def multiple_ellipses(x: int) -> float:
     ...
     ...  # Y010 Function body must contain only "..."
+
+async def empty_async(x: int) -> float:
+    ...
+
+async def passing_async(x: int) -> float:
+    pass  # Y009 Empty body should contain "...", not "pass"
+
+async def raising_async(x: int) -> float:
+    raise TypeError
+
+async def returning_async(x: int) -> float:
+    return x / 2  # Y010 Function body must contain only "..."
+
+async def multiple_ellipses_async(x: int) -> float:
+    ...
+    ...  # Y010 Function body must contain only "..."
