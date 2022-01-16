9ccb648145f58439500c243ff3abc9ce9cd989fb
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2022-01-16 07:57:48 -0800

    update tests

--- tests/emptyfunctions.pyi
+++ tests/emptyfunctions.pyi
@@ -5,15 +5,15 @@ def passing(x: int) -> float:
     pass  # Y009 Empty body should contain "...", not "pass"
 
 def raising(x: int) -> float:
-    raise TypeError
-
-class GoodClass:
-    def __init__(self, x: int) -> None:
-        self.x = x
+    raise TypeError  # Y010 Function body must contain only "..."
 
 class BadClass:
-    def __init__(self, b: GoodClass, x: int) -> None:
-        b.x = x
+    def __init__(self, x: int) -> None:
+        self.x = x  # Y010 Function body must contain only "..."
+
+class WorseClass:
+    def __init__(self, b: BadClass, x: int) -> None:
+        b.x = x  # Y010 Function body must contain only "..."
 
 def returning(x: int) -> float:
     return x / 2  # Y010 Function body must contain only "..."
@@ -29,7 +29,7 @@ async def passing_async(x: int) -> float:
     pass  # Y009 Empty body should contain "...", not "pass"
 
 async def raising_async(x: int) -> float:
-    raise TypeError
+    raise TypeError  # Y010 Function body must contain only "..."
 
 async def returning_async(x: int) -> float:
     return x / 2  # Y010 Function body must contain only "..."
