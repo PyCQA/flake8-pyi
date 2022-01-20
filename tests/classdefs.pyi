0e434ef4288dc846edf56c22ba4676dedfb81f17
Alex Waygood <Alex.Waygood@Gmail.com>
2022-01-20 15:20:37 +0000

    Take out 3.7-incompatible test

--- tests/classdefs.pyi
+++ tests/classdefs.pyi
@@ -8,14 +8,6 @@ class Bad:
         ...
 
 
-class AlsoBad:
-    def __repr__(self, /) -> str:  # Y029 Defining __repr__ or __str__ in a stub is almost always redundant
-        ...
-
-    def __str__(self, /) -> str:  # Y029 Defining __repr__ or __str__ in a stub is almost always redundant
-        ...
-
-
 class Good:
     @abstractmethod
     def __str__(self) -> str:
