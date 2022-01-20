6b0ad4ab6b2bf52045ca47ec69995040e41659f6
Akuli <akuviljanen17@gmail.com>
2022-01-20 21:08:57 +0200

    Add test with multiple Literal members

--- tests/union_duplicates.pyi
+++ tests/union_duplicates.pyi
@@ -28,3 +28,4 @@ just_literals_subscript_union: Union[Literal[1], Literal[2]]  # Y030 Multiple Li
 mixed_subscript_union: Union[str, Literal['foo'], Literal['bar']]  # Y030 Multiple Literal members in a union. Combine them into one, e.g. "Literal['foo', 'bar']".
 just_literals_pipe_union: TypeAlias = Literal[True] | Literal['idk']  # Y030 Multiple Literal members in a union. Use a single Literal, e.g. "Literal[True, 'idk']".
 mixed_pipe_union: TypeAlias = Union[Literal[966], int, Literal['baz']]  # Y030 Multiple Literal members in a union. Combine them into one, e.g. "Literal[966, 'baz']".
+many_literal_members_but_needs_combining: TypeAlias = int | Literal['a', 'b'] | Literal['baz']  # Y030 Multiple Literal members in a union. Combine them into one, e.g. "Literal['a', 'b', 'baz']".
