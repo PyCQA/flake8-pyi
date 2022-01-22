f124c9c4be17695b75fb85310ee5b9789e7e0758
Alex Waygood <Alex.Waygood@Gmail.com>
2022-01-22 14:26:34 +0000

    Screw things up to test pre-commit still works

--- pyi.py
+++ pyi.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 from __future__ import annotations
 
+from collections import Counter
 import argparse
 import ast
 import logging
 import optparse
 import re
 import sys
-from collections import Counter
 from collections.abc import Iterable, Iterator, Sequence
 from contextlib import contextmanager
 from copy import deepcopy
@@ -28,6 +28,8 @@ from pyflakes.checker import (  # type: ignore[import]
 
 if sys.version_info >= (3, 9):
     from ast import unparse
+
+
 else:
     from ast_decompiler import decompile as unparse
 
