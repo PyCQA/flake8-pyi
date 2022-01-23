c39805f899e1a443fd3e2a6c79383e28cf3745a2
Alex Waygood <Alex.Waygood@Gmail.com>
2022-01-23 15:26:26 +0000

    Reformat a few test files

--- tests/vanilla_flake8_not_clean_forward_refs.pyi
+++ tests/vanilla_flake8_not_clean_forward_refs.pyi
@@ -3,10 +3,6 @@ from typing import TypeAlias, Union
 
 ManyStr: TypeAlias = list[EitherStr]  # F821 undefined name 'EitherStr'
 EitherStr: TypeAlias = Union[str, bytes]
-
-
-def function(accepts: EitherStr) -> None:
-    ...
-
+def function(accepts: EitherStr) -> None: ...
 
 del EitherStr  # private name, not exported
