179fbd0917fdbe88bb3c3880ae8c71f9d351eac2
AlexWaygood <alex.waygood@gmail.com>
2022-06-21 16:27:56 +0100

    Y026: error for any assignment to a subscripted object

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,9 +8,8 @@ Features:
 * Improve error message for Y026 check.
 * Expand Y026 check. Since version 22.4.0, this has only emitted an error for
   assignments to `typing.Literal`, `typing.Union`, and PEP 604 unions. It now also
-  emits an error for assignments to `typing.Annotated`, `typing.Optional` and
-  `typing.Any`, as well as subscripted `tuple`s, `dict`s, `set`s, `frozenset`s,
-  `list`s, and `type`s.
+  emits an error for any subscription on the right-hand side of a simple assignment, as
+  well as for assignments to `typing.Any`.
 * Support Python 3.11.
 * Support `typing_extensions.overload` and `typing_extensions.NamedTuple`. (The latter
   is not yet released, but will be in the next version of `typing_extensions`.)
--- pyi.py
+++ pyi.py
@@ -151,21 +151,6 @@ _BAD_Y027_IMPORTS = {
     "Container": "T",
 }
 
-_Y026_TYPEALIAS_BLACKLIST = frozenset(
-    {
-        "Literal",
-        "Union",
-        "Optional",
-        "Annotated",
-        "dict",
-        "tuple",
-        "list",
-        "set",
-        "frozenset",
-        "type",
-    }
-)
-
 
 class PyiAwareFlakesChecker(FlakesChecker):
     def deferHandleNode(self, node: ast.AST | None, parent) -> None:
@@ -838,17 +823,6 @@ class PyiVisitor(ast.NodeVisitor):
         else:
             self.visit(value)
 
-    def _Y026_error(
-        self, node: ast.Assign, target: ast.Name, assignment: ast.expr
-    ) -> None:
-        new_node = ast.AnnAssign(
-            target=target,
-            annotation=ast.Name(id="TypeAlias", ctx=ast.Load()),
-            value=assignment,
-            simple=1,
-        )
-        self.error(node, Y026.format(suggestion=unparse(new_node)))
-
     def _check_for_type_aliases(
         self, node: ast.Assign, target: ast.Name, assignment: ast.expr
     ) -> None:
@@ -860,22 +834,22 @@ class PyiVisitor(ast.NodeVisitor):
         unusual cases where assignment to the result of a call is legitimate
         in stubs (`T = TypeVar("T")`, `List = _Alias()`, etc.).
 
-        We also avoid triggering Y026 for aliases like `X = Any` or `Y = str`.
+        We also avoid triggering Y026 for aliases like `X = str`.
         It's ultimately nearly impossible to reliably detect
         whether these are type aliases or variable aliases,
         unless you're a type checker (and we're not).
+
+        The only exception (which we special-case, because it is so common in a stub)
+        is the type alias `X = (typing.)Any`.
         """
-        if isinstance(assignment, ast.BinOp) or _is_Any(assignment):
-            return self._Y026_error(node, target, assignment)
-        if not isinstance(assignment, ast.Subscript):
-            return
-
-        name_if_from_builtins_or_typing = _get_name_of_class_if_from_modules(
-            assignment.value, modules=_TYPING_MODULES | {"builtins"}
-        )
-
-        if name_if_from_builtins_or_typing in _Y026_TYPEALIAS_BLACKLIST:
-            self._Y026_error(node, target, assignment)
+        if isinstance(assignment, (ast.Subscript, ast.BinOp)) or _is_Any(assignment):
+            new_node = ast.AnnAssign(
+                target=target,
+                annotation=ast.Name(id="TypeAlias", ctx=ast.Load()),
+                value=assignment,
+                simple=1,
+            )
+            self.error(node, Y026.format(suggestion=unparse(new_node)))
 
     def visit_Name(self, node: ast.Name) -> None:
         self.all_name_occurrences[node.id] += 1
