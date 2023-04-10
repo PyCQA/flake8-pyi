0926ee46082a301785a61df9032656e8b3dd2169
Tomas Roun <tomas.roun8@gmail.com>
2023-04-11 00:40:11 +0200

    Add more tests

--- tests/union_duplicates.pyi
+++ tests/union_duplicates.pyi
@@ -2,26 +2,44 @@
 import builtins
 import typing
 from collections.abc import Mapping
-from typing import Union
+from typing import Union, Type  # noqa: Y022
 
 import typing_extensions
-from typing_extensions import Literal, TypeAlias
+from typing_extensions import Literal, TypeAlias, Type as Type_  # noqa: Y022
 
 def f1_pipe(x: int | str) -> None: ...
 def f2_pipe(x: int | int) -> None: ...  # Y016 Duplicate union member "int"
 def f3_pipe(x: None | int | int) -> None: ...  # Y016 Duplicate union member "int"
 def f4_pipe(x: int | None | int) -> None: ...  # Y016 Duplicate union member "int"
 def f5_pipe(x: int | int | None) -> None: ...  # Y016 Duplicate union member "int"
-def f6_pipe(x: type[int] | type[str] | type[float]) -> None: ...  # Y055 Multiple type builtins in a union. Use a single type expression, e.g. "type[int | str | float]".
-def f7_pipe(x: type[int] | str | type[float]) -> None: ...  # Y055 Multiple type builtins in a union. Combine them into one, e.g. "type[int | float]".
+def f6_pipe(x: type[int] | type[str] | type[float]) -> None: ...  # Y055 Multiple "type[C]" members in a union. Use a single type expression, e.g. "type[int | str | float]".
+def f7_pipe(x: type[int] | str | type[float]) -> None: ...  # Y055 Multiple "type[C]" members in a union. Combine them into one, e.g. "type[int | float]".
+def f8_pipe(x: builtins.type[int] | builtins.type[str] | builtins.type[float]) -> None: ...  # Y055 Multiple "type[C]" members in a union. Use a single type expression, e.g. "type[int | str | float]".
+def f9_pipe(x: builtins.type[int] | str | builtins.type[float]) -> None: ...  # Y055 Multiple "type[C]" members in a union. Combine them into one, e.g. "type[int | float]".
+def f10_pipe(x: type[int] | builtins.type[float]) -> None: ...  # Y055 Multiple "type[C]" members in a union. Use a single type expression, e.g. "type[int | float]".
+# typing.Type and typing_extensions.Type are intentionally excluded from Y055
+# The following type annotations should not generate any Y055 errors
+def f11_pipe(x: Type[int] | Type[str]) -> None: ...
+def f12_pipe(x: typing.Type[int] | typing.Type[str]) -> None: ...  # noqa: Y022
+def f13_pipe(x: Type_[int] | Type_[str]) -> None: ...
+def f14_pipe(x: typing_extensions.Type[int] | typing_extensions.Type[str]) -> None: ...  # noqa: Y022
 
 def f1_union(x: Union[int, str]) -> None: ...
 def f2_union(x: Union[int, int]) -> None: ...  # Y016 Duplicate union member "int"
 def f3_union(x: Union[None, int, int]) -> None: ...  # Y016 Duplicate union member "int"
 def f4_union(x: typing.Union[int, None, int]) -> None: ...  # Y016 Duplicate union member "int"
 def f5_union(x: typing.Union[int, int, None]) -> None: ...  # Y016 Duplicate union member "int"
-def f6_union(x: Union[type[int], type[str], type[float]]) -> None: ...  # Y055 Multiple type builtins in a union. Use a single type expression, e.g. "type[Union[int, str, float]]".
-def f7_union(x: Union[type[int], str, type[float]]) -> None: ...  # Y055 Multiple type builtins in a union. Combine them into one, e.g. "type[Union[int, float]]".
+def f6_union(x: Union[type[int], type[str], type[float]]) -> None: ...  # Y055 Multiple "type[C]" members in a union. Use a single type expression, e.g. "type[Union[int, str, float]]".
+def f7_union(x: Union[type[int], str, type[float]]) -> None: ...  # Y055 Multiple "type[C]" members in a union. Combine them into one, e.g. "type[Union[int, float]]".
+def f8_union(x: Union[builtins.type[int], builtins.type[str], builtins.type[float]]) -> None: ...  # Y055 Multiple "type[C]" members in a union. Use a single type expression, e.g. "type[Union[int, str, float]]".
+def f9_union(x: Union[builtins.type[int], str, builtins.type[float]]) -> None: ...  # Y055 Multiple "type[C]" members in a union. Combine them into one, e.g. "type[Union[int, float]]".
+def f10_union(x: Union[type[int], builtins.type[float]]) -> None: ...  # Y055 Multiple "type[C]" members in a union. Use a single type expression, e.g. "type[Union[int, float]]".
+# typing.Type and typing_extensions.Type are intentionally excluded from Y055
+# The following type annotations should not generate any Y055 errors
+def f11_union(x: Union[Type[int], Type[str]]) -> None: ...
+def f12_union(x: Union[typing.Type[int], typing.Type[str]]) -> None: ...  # noqa: Y022
+def f13_union(x: Union[Type_[int], Type_[str]]) -> None: ...
+def f14_union(x: Union[typing_extensions.Type[int], typing_extensions.Type[str]]) -> None: ...  # noqa: Y022
 
 just_literals_subscript_union: Union[Literal[1], typing.Literal[2]]  # Y030 Multiple Literal members in a union. Use a single Literal, e.g. "Literal[1, 2]".
 mixed_subscript_union: Union[bytes, Literal['foo'], typing_extensions.Literal['bar']]  # Y030 Multiple Literal members in a union. Combine them into one, e.g. "Literal['foo', 'bar']".
@@ -60,4 +78,6 @@ class Four:
 DupesHereSoNoY051: TypeAlias = int | int | Literal[42]  # Y016 Duplicate union member "int"
 NightmareAlias1 = int | float | Literal[4, b"bar"] | Literal["foo"]  # Y026 Use typing_extensions.TypeAlias for type aliases, e.g. "NightmareAlias1: TypeAlias = int | float | Literal[4, b'bar'] | Literal['foo']"  # Y030 Multiple Literal members in a union. Combine them into one, e.g. "Literal[4, b'bar', 'foo']".  # Y051 "Literal[4]" is redundant in a union with "int"
 nightmare_alias2: TypeAlias = int | float | Literal[True, 4] | Literal["foo"]  # Y042 Type aliases should use the CamelCase naming convention  # Y030 Multiple Literal members in a union. Combine them into one, e.g. "Literal[True, 4, 'foo']".  # Y051 "Literal[4]" is redundant in a union with "int"
-DoublyNestedAlias: TypeAlias = Union[type[str], type[float] | type[bytes]]  # Y055 Multiple type builtins in a union. Use a single type expression, e.g. "type[float | bytes]".
+DoublyNestedAlias: TypeAlias = Union[type[str], type[float] | type[bytes]]  # Y055 Multiple "type[C]" members in a union. Use a single type expression, e.g. "type[float | bytes]".
+# typing.Type and typing_extensions.Type are intentionally excluded from Y055
+DoublyNestedAlias2: TypeAlias = Union[Type[str], typing.Type[float], Type_[bytes], typing_extensions.Type[complex]]  # noqa: Y022
