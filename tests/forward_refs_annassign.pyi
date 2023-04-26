713c67fc8be10dcd6a163c06dc086e8625b0e3c9
Tomas Roun <tomas.roun8@gmail.com>
2023-04-26 15:49:49 +0200

    Fix typos

--- tests/forward_refs_annassign.pyi
+++ tests/forward_refs_annassign.pyi
@@ -39,11 +39,11 @@ class B:
     foo: A
     bar: dict[str, A]
 
-# This recursive definitions is allowed.
+# This recursive definition is allowed.
 # We allow it by disabling checks for anything in the subscript
 class Leaf: ...
 class Tree(list[Tree | Leaf]): ...
 
-# This recursive definition is not allowed
+# This recursive definition is not allowed.
 class Parent(Child): ...   # F821 undefined name 'Child'
 class Child(Parent): ...
