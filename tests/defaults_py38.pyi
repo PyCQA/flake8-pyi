7b07256318486f4ba8610f478af0a2d885a275b0
Alex Waygood <Alex.Waygood@Gmail.com>
2023-06-17 20:14:36 +0100

    Drop support for Python 3.7 (#393)
    Part (1) of #392

--- tests/defaults_py38.pyi
+++
@@ -1,7 +0,0 @@
-import os
-
-def f(x: "int", /) -> None: ...  # Y020 Quoted annotations should never be used in stubs
-def f1(x: int, /) -> None: ...
-def f2(x: int, /, y: "int") -> None: ...  # Y020 Quoted annotations should never be used in stubs
-def f3(x: str = "y", /) -> None: ...
-def f4(x: str = os.pathsep, /) -> None: ...  # Y011 Only simple default values allowed for typed arguments
