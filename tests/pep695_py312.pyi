c3892a60460691912346499c347c7e1f3325cadf
Alex Waygood <Alex.Waygood@Gmail.com>
2023-06-17 20:15:44 +0100

    Update tests/pep695_py312.pyi
    Co-authored-by: Jelle Zijlstra <jelle.zijlstra@gmail.com>

--- tests/pep695_py312.pyi
+++ tests/pep695_py312.pyi
@@ -1,5 +1,8 @@
 type lowercase_alias = str | int  # Y042 Type aliases should use the CamelCase naming convention
 type _LooksLikeATypeVarT = str | int  # Y043 Bad name for a type alias (the "T" suffix implies a TypeVar)
 type _Unused = str | int  # Y047 Type alias "_Unused" is not used
+type _List[T] = list[T]
+
+y: _List[int]
 
 x: _LooksLikeATypeVarT
