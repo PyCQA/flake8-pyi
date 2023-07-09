2a86db8271dc500247a8a69419536240334731cf
Alex Waygood <Alex.Waygood@Gmail.com>
2023-07-09 16:39:59 +0100

    Add a warning for `tuple[int]` annotations (#410)
    Fixes #131
    
    The error code is disabled by default, so as to avoid false positives.
    If this PR is merged, I can work on creating a non-blocking
    mypy_primer-esque workflow for typeshed (which runs flake8 only on
    changed files, with `--extend-select=Y090`, and posts a comment showing
    the new errors) to flag potential issues in PRs.
    
    Co-authored-by: Jelle Zijlstra <jelle.zijlstra@gmail.com>

---
+++ tests/single_element_tuples.pyi
@@ -0,0 +1,8 @@
+# flags: --extend-select=Y090
+import builtins
+import typing
+
+a: tuple[int]  # Y090 "tuple[int]" means "a tuple of length 1, in which the sole element is of type 'int'". Perhaps you meant "tuple[int, ...]"?
+b: typing.Tuple[builtins.str]  # Y022 Use "tuple[Foo, Bar]" instead of "typing.Tuple[Foo, Bar]" (PEP 585 syntax)  # Y090 "typing.Tuple[builtins.str]" means "a tuple of length 1, in which the sole element is of type 'builtins.str'". Perhaps you meant "typing.Tuple[builtins.str, ...]"?
+c: tuple[int, ...]
+d: typing.Tuple[builtins.str, builtins.complex]  # Y022 Use "tuple[Foo, Bar]" instead of "typing.Tuple[Foo, Bar]" (PEP 585 syntax)
