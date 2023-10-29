e22520a131a489ce986951355c51aa9a5046e4b5
Tomas Roun <tomas.roun8@gmail.com>
2023-10-29 17:57:59 +0100

    Improve tests

--- tests/literals.pyi
+++ tests/literals.pyi
@@ -2,5 +2,4 @@ from typing import Literal
 
 Literal[None]  # Y061 None inside "Literal[]" expression. Replace with "None"
 Literal[True, None]  # Y061 None inside "Literal[]" expression. Replace with "Literal[True] | None"
-Literal[1, None, "foo"]  # Y061 None inside "Literal[]" expression. Replace with "Literal[1, 'foo'] | None"
-Literal[None, None]  # Y061 None inside "Literal[]" expression. Replace with "Literal[None] | None"
+Literal[1, None, "foo", None]  # Y061 None inside "Literal[]" expression. Replace with "Literal[1, 'foo', None] | None"
