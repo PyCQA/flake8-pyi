cd306232e8f267c3ba70491f768d4bf757269143
Tomas Roun <tomas.roun8@gmail.com>
2024-03-16 15:32:45 +0100

    Add tests

--- tests/literals.pyi
+++ tests/literals.pyi
@@ -1,4 +1,4 @@
-from typing import Literal
+from typing import Final, Literal
 
 Literal[None]  # Y061 None inside "Literal[]" expression. Replace with "None"
 Literal[True, None]  # Y061 None inside "Literal[]" expression. Replace with "Literal[True] | None"
@@ -21,3 +21,8 @@ Literal[1, None, "foo", None]  # Y061 None inside "Literal[]" expression. Replac
 # and there are no None members in the Literal[] slice,
 # only emit Y062:
 Literal[None, True, None, True]  # Y062 Duplicate "Literal[]" member "True"
+
+x: Final[Literal[True]]  # Y064 Use "x: Final = True" instead of "x: Final[Literal[True]]"
+# If Y061 and Y064 both apply, only emit Y064
+y: Final[Literal[None]]  # Y064 Use "y: Final = None" instead of "y: Final[Literal[None]]"
+z: Final[Literal[True, False]]
