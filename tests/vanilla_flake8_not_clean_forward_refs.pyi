43a00d7eb2310ce695d08c854ff245e8fc6c65b0
Sebastian Rittau <srittau@rittau.biz>
2025-08-29 18:04:24 +0200

    Fix

--- tests/vanilla_flake8_not_clean_forward_refs.pyi
+++ tests/vanilla_flake8_not_clean_forward_refs.pyi
@@ -1,8 +1,12 @@
 # flags: --no-pyi-aware-file-checker --extend-ignore=Y037
+import sys
 from typing import TypeAlias, Union
 
 ManyStr: TypeAlias = list[EitherStr]  # F821 undefined name 'EitherStr'
 EitherStr: TypeAlias = Union[str, bytes]
-def function(accepts: EitherStr) -> None: ...  # F821 undefined name 'EitherStr'
+if sys.version_info >= (3, 14):
+    def function(accepts: EitherStr) -> None: ...  # F821 undefined name 'EitherStr'
+else:
+    def function(accepts: EitherStr) -> None: ...
 
 del EitherStr  # private name, not exported
